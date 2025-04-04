1971cd5be234a300992363e42e7b14116e8c53ce
Daniela Pech <randakova.daniela@gmail.com>
2025-04-04 22:05:27 +0200

    vypracovane cviceni - specialni druhy funkci javascript

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const goodbye = (name) => {
+  const byeElm = document.querySelector('.email__closing');
+  byeElm.textContent = 'Na shledanou ' + name;
+};
+goodbye('Pavel Veliký');
 
+const fillSubject = (subject) => {
+  const sbjElm = document.querySelector('.email__subject');
+  sbjElm.textContent = subject;
+};
+fillSubject('Nový název e-mailu');
+
+const fillBody = (body) => {
+  const bodyElm = document.querySelector('.email__body');
+  bodyElm.textContent = body;
+};
+fillBody(
+  'Nové tělo e-mailu  tělo e-mailu  tělo e-mailu  tělo e-mailu  tělo e-mailu  tělo e-mailu  tělo e-mailu  tělo e-mailu  tělo e-mailu  tělo e-mailu  tělo e-mailu  tělo e-mailu  tělo e-mailu ',
+);
